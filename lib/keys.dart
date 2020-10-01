8a3dc196e8cb8c5b799c949c626c56066dbbf61b
Madhav Tripathi <madhav.37.tripathi@gmail.com>
2020-10-01 07:17:32 +0530

    workflow try 2

---
+++ lib/keys.dart
@@ -0,0 +1,2 @@
+//! Kindly Generate your own key from: https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/microsoft-text-analytics1
+const RAPID_API_KEY = "b46fe7e5aemsheb474bceeb6c122p1a9f2ajsnc0862f1df45a";
